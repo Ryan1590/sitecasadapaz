686f40a8ba85060af709328d9cbdce94710de030
Ryan1590 <ryanneno1590@gmail.com>
2024-10-08 12:30:55 -0300

    Cor Footer black

--- front-end/app/pages/Rodape/page.tsx
+++ front-end/app/pages/Rodape/page.tsx
@@ -1,6 +1,6 @@
 const Rodape = () => {
     return (
-      <footer className="bg-dark text-center text-white p-3 mt-auto">
+      <footer className="bg-black text-center text-white p-3 mt-auto">
         <p>&copy; 2024 Casa da Paz. Todos os direitos reservados.</p>
       </footer>
     );
