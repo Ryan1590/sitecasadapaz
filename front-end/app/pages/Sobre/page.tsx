7b562ad7d221969f3c7870999ccd3445547dbbf3
Ryan Rodrigues <ryanneno1590@gmail.com>
2024-10-12 21:30:32 -0300

    tela sobre responsividade e layount

--- front-end/app/pages/Sobre/page.tsx
+++ front-end/app/pages/Sobre/page.tsx
@@ -28,6 +28,7 @@ const Sobre = () => {
   const [sobre, setSobre] = useState<SobreData[]>([]);
   const [equipe, setEquipe] = useState<MembroEquipe[]>([]);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [displayCount, setDisplayCount] = useState(4); // Exibe até 4 membros
 
   useEffect(() => {
     const fetchSobre = async () => {
@@ -59,87 +60,87 @@ const Sobre = () => {
     fetchEquipe();
   }, []);
 
+  const updateDisplayCount = () => {
+    if (window.innerWidth < 768) {
+      setDisplayCount(1); 
+    } else {
+      setDisplayCount(4);
+    }
+  };
+
+  useEffect(() => {
+    updateDisplayCount();
+
+    window.addEventListener("resize", updateDisplayCount);
+    return () => window.removeEventListener("resize", updateDisplayCount);
+  }, []);
+
   const nextSlide = () => {
-    setCurrentSlide(
-      (prevSlide) => (prevSlide + 1) % Math.ceil(equipe.length / 4)
+    setCurrentSlide((prevSlide) =>
+      (prevSlide + 1) % Math.ceil(equipe.length / displayCount)
     );
   };
 
   const prevSlide = () => {
-    setCurrentSlide(
-      (prevSlide) =>
-        (prevSlide - 1 + Math.ceil(equipe.length / 4)) %
-        Math.ceil(equipe.length / 4)
+    setCurrentSlide((prevSlide) =>
+      (prevSlide - 1 + Math.ceil(equipe.length / displayCount)) % Math.ceil(equipe.length / displayCount)
     );
   };
 
-  const startIndex = currentSlide * 4;
-  const displayedMembers = equipe.slice(startIndex, startIndex + 4);
+  const startIndex = currentSlide * displayCount;
+  const displayedMembers = equipe.slice(startIndex, startIndex + displayCount);
 
   useEffect(() => {
-    const interval = setInterval(nextSlide, 3000);
-    return () => clearInterval(interval);
-  }, [equipe.length]);
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => clearInterval(interval); 
+  }, [currentSlide, displayCount, equipe.length]);
 
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
 
       {/* Banner */}
-      <div className="text-center banner-container">
-        <img
-          src={`data:image/jpeg;base64,${
-            sobre[0]?.banner_principal || "Carregando..."
-          }`}
-          alt="Banner da Casa da Paz"
-          className="img-fluid"
-        />
-      </div>
+      <div className="text-center banner-container fade-in" style={{ marginTop: '70px' }}>
+      <img
+        src={`data:image/jpeg;base64,${sobre[0]?.banner_principal || "Carregando Banner..."}`}
+        alt="Banner da Casa da Paz"
+        className="img-fluid"
+      />
+    </div>
 
       <main className="flex-grow-1 d-flex flex-column align-items-center text-dark mt-5">
         <div className="container">
           {/* Seção sobre a Casa da Paz */}
-          <div className="row mb-5">
+          <div className="row mb-5 fade-in">
             <div className="col-md-6">
               <img
-                src={`data:image/jpeg;base64,${
-                  sobre[0]?.imagem_missao || "Carregando..."
-                }`}
-                alt="Descrição da imagem"
-                className="img-fluid rounded-circle mb-4 shadow"
+                src={`data:image/jpeg;base64, ${sobre[0]?.imagem_missao || "Carregando..."}`}
+                alt="Imagem missão"
+                className="img-fluid mb-4 shadow"
               />
             </div>
             <div className="col-md-6">
               <div className="card mb-4">
                 <div className="card-body">
                   <h2 className="card-title">
-                    <i className="fas fa-bullseye text-primary mr-2"></i> Nossa
-                    Missão
+                    <i className="fas fa-bullseye text-primary mr-2"></i> Nossa Missão
                   </h2>
-                  <p className="text-muted">
-                    {sobre[0]?.missao || "Carregando..."}
-                  </p>
+                  <p className="text-muted">{sobre[0]?.missao || "Carregando..."}</p>
                 </div>
               </div>
               <div className="card mb-4">
                 <div className="card-body">
                   <h2 className="card-title">
-                    <i className="fas fa-heart text-primary mr-2"></i> Nossos
-                    Valores
+                    <i className="fas fa-heart text-primary mr-2"></i> Nossos Valores
                   </h2>
                   <ul className="list-unstyled">
-                    <li>
-                      <i className="fas fa-hand-holding-heart"></i> Empatia
-                    </li>
-                    <li>
-                      <i className="fas fa-check-circle"></i> Respeito
-                    </li>
-                    <li>
-                      <i className="fas fa-shield-alt"></i> Transparência
-                    </li>
-                    <li>
-                      <i className="fas fa-users"></i> Colaboração
-                    </li>
+                    <li><i className="fas fa-hand-holding-heart"></i> Empatia</li>
+                    <li><i className="fas fa-check-circle"></i> Respeito</li>
+                    <li><i className="fas fa-shield-alt"></i> Transparência</li>
+                    <li><i className="fas fa-users"></i> Colaboração</li>
                   </ul>
                 </div>
               </div>
@@ -147,50 +148,40 @@ const Sobre = () => {
           </div>
 
           <section className="my-5">
-            <div className="row">
+            <div className="row fade-in">
               <div className="col-md-12">
+                {/* Seção sobre a Casa da paz */}
                 <div className="card mb-4">
                   <div className="card-body">
                     <h3 className="mt-4 text-primary">
-                      <i className="fas fa-info-circle mr-2"></i> Sobre a Casa
-                      da Paz
+                      <i className="fas fa-info-circle mr-2"></i> Sobre a Casa da Paz
                     </h3>
-                    <p className="text-muted">
-                      {sobre[0]?.sobre || "Carregando..."}
-                    </p>
+                    <p className="text-muted">{sobre[0]?.sobre || "Carregando..."}</p>
+                  </div>
+                </div>
+
+                <div className="card mb-4">
+                  <div className="card-body">
+                    <h3 className="mt-4 text-primary">
+                      <i className="fas fa-clipboard-list mr-2"></i> No que acreditamos?
+                    </h3>
+                    <p className="text-muted">{sobre[0]?.no_que_acreditamos || "Carregando..."}</p>
                   </div>
                 </div>
                 <div className="card mb-4">
                   <div className="card-body">
                     <h3 className="mt-4 text-primary">
-                      <i className="fas fa-clipboard-list mr-2"></i> No que
-                      acreditamos?
+                      <i className="fas fa-school mr-2"></i> Onde e como as atividades acontecem?
                     </h3>
-                    <p className="text-muted">
-                      {sobre[0]?.no_que_acreditamos || "Carregando..."}
-                    </p>
+                    <p className="text-muted">{sobre[0]?.atividades || "Carregando..."}</p>
                   </div>
                 </div>
                 <div className="card mb-4">
                   <div className="card-body">
                     <h3 className="mt-4 text-primary">
-                      <i className="fas fa-school mr-2"></i> Onde e como as
-                      atividades acontecem?
+                      <i className="fas fa-dollar-sign mr-2"></i> De onde vêm os recursos?
                     </h3>
-                    <p className="text-muted">
-                      {sobre[0]?.atividades || "Carregando..."}
-                    </p>
-                  </div>
-                </div>
-                <div className="card mb-4">
-                  <div className="card-body">
-                    <h3 className="mt-4 text-primary">
-                      <i className="fas fa-dollar-sign mr-2"></i> De onde vêm os
-                      recursos?
-                    </h3>
-                    <p className="text-muted">
-                      {sobre[0]?.recursos || "Carregando..."}
-                    </p>
+                    <p className="text-muted">{sobre[0]?.recursos || "Carregando..."}</p>
                   </div>
                 </div>
                 <div className="card mb-4">
@@ -198,9 +189,7 @@ const Sobre = () => {
                     <h3 className="mt-4 text-primary">
                       <i className="fas fa-building mr-2"></i> Sede própria!
                     </h3>
-                    <p className="text-muted">
-                      {sobre[0]?.sede || "Carregando..."}
-                    </p>
+                    <p className="text-muted">{sobre[0]?.sede || "Carregando..."}</p>
                   </div>
                 </div>
               </div>
@@ -214,25 +203,18 @@ const Sobre = () => {
               &lt;
             </button>
 
-            <div className="team-members d-flex justify-content-center">
-              {displayedMembers.map((membro, index) => (
-                <div
-                  key={membro.id}
-                  className={`car-control mb-4 shadow-sm border-light member-card ${
-                    index > 0 ? "d-none d-md-block" : ""
-                  }`}
-                >
-                  <div className="member-image-container">
-                    <img
-                      src={membro.foto}
-                      className="card-img-top"
-                      alt={membro.nome}
-                    />
-                  </div>
-                  <div className="card-body">
-                    <h5 className="card-title text-primary">{membro.nome}</h5>
-                    <p className="card-text">{membro.cargo}</p>
-                    <p className="card-text text-muted">{membro.profissao}</p>
+            <div className="team-members">
+              {displayedMembers.map((membro) => (
+                <div key={membro.id} className="member-card mb-4 mx-2">
+                  <div className="card" style={{ width: "13rem" }}>
+                    <div className="member-image-container">
+                      <img src={membro.foto} className="card-img-top" alt={membro.nome} />
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title text-primary">{membro.nome}</h5>
+                      <p className="card-text">{membro.cargo}</p>
+                      <p className="card-text text-muted">{membro.profissao}</p>
+                    </div>
                   </div>
                 </div>
               ))}
@@ -244,6 +226,7 @@ const Sobre = () => {
           </div>
         </div>
       </main>
+
       <Rodape />
     </div>
   );
