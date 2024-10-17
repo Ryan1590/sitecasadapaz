bcd2fc54f5320f7f0d868bb11e315d5581d9cd47
Ryan Rodrigues <ryanneno1590@gmail.com>
2024-10-17 20:16:06 -0300

    back-end pagina de doação controller e model

--- Back-end/routes/web.php
+++ Back-end/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\NossaEquipeController;
 use App\Http\Controllers\SobrenosController;
 use App\Http\Controllers\ContatosController;
+use App\Http\Controllers\DoacaoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,5 @@ Route::get('api/sobre', [SobrenosController::class, 'index']);
 Route::get('api/nossaequipe', [NossaEquipeController::class, 'index']);
 /* api contatos*/
 Route::get('api/contatos', [ContatosController::class, 'index']);
+/* api doacao*/
+Route::get('api/doacao', [DoacaoController::class, 'index']);
